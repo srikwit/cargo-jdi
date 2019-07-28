7cf2048caf037b6af255f2e890d2e7add8ee93f5
Srikanth Suresh <4140019+srikwit@users.noreply.github.com>
2019-07-28 18:33:44 +0530

    Just Do it!

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use rand::Rng;
+fn main() {
+    let motivate = [
+    "Do it",
+    "Don't let your dreams be dreams",
+    "Just do it",
+    "If you're tired of starting over, Stop giving up",
+    "Make your dreams come true",
+    "Nothing is impossible",
+    "No, what are you waiting for?",
+    "Some people dream of success, While you're gonna wake up and work hard at it",
+    "You should get to the point\nWhere anyone else would quit\nAnd you're not going to stop there",
+    "Yes you can",
+    "Yesterday you said tomorrow"
+];
+    let mut rng = rand::thread_rng();
+    match motivate.is_empty() {
+        true => println!("Out of Energy!"),
+        false => println!("{}", motivate[rng.gen_range(0, motivate.len())]),
+    }
+}
